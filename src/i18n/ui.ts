39a762a727750c2d5ec24968076ae4283e301c05
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2025-04-01 23:06:00 -0600

    i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -16,6 +16,8 @@ export const ui = {
     'nav.works': 'Selected Work',
     'nav.experience': 'Experience',
     'nav.contact': 'Contact',
+    'hero.title': 'Developer dedicated to pixel-perfect UI & smooth digital experiences.',
+    'hero.cta':`Let's talk`,
     'about.title': 'About me',
     'about.first-description': `With 5+ years of experience, I’ve contributed to fast-moving startups and
       large-scale platforms. My sweet spot is modern frontend development
@@ -32,6 +34,8 @@ export const ui = {
     'nav.works': 'Trabajos',
     'nav.experience': 'Experiencia',
     'nav.contact': 'Contacto',
+    'hero.title': 'Desarrollador enfocado en interfaces pixel-perfect y experiencias digitales fluidas.',
+    'hero.cta':`Hablemos`,
     'about.title': 'Sobre mi',
     'about.first-description': `Con más de 5 años de experiencia, he contribuido en startups de rápido crecimiento y plataformas a gran escala. Mi especialidad es el desarrollo frontend moderno (React, Astro, TypeScript), aunque también me siento cómodo trabajando en el backend cuando es necesario.`,
     'about.first-small-description': 'A los diseñadores les agrada trabajar conmigo porque cuido los detalles. Los PMs confían en mí porque cumplo con los plazos. Los ingenieros me aprecian porque mis pull requests no rompen nada',
