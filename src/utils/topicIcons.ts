815ad35900737ed36cd89d8c5770e86e45973cae
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2025-04-22 21:55:32 -0600

    changes

--- src/utils/topicIcons.ts
+++ src/utils/topicIcons.ts
@@ -1,12 +1,22 @@
+// export const topicIcons: Record<string, string> = {
+//   react: "⚛️",
+//   vite: "⚡",
+//   astro: "🪐",
+//   webcomponents: "🧩",
+//   expressjs: "🚂",
+//   backend: "🖥️",
+//   frontend: "🎨",
+//   typeScript: "🔷",
+//   api: "🔌",
+//   storybook: "📘",
+// };
+
+
 export const topicIcons: Record<string, string> = {
-  react: "⚛️",
-  vite: "⚡",
-  astro: "🪐",
-  webcomponents: "🧩",
-  expressjs: "🚂",
-  backend: "🖥️",
-  frontend: "🎨",
-  typeScript: "🔷",
-  api: "🔌",
-  storybook: "📘",
+  react: "/icons/react.svg",
+  vite: "/icons/vite.svg",
+  astro: "/icons/astro.svg",
+  webcomponents: "/icons/webcomponents.svg",
+  expressjs:"/icons/express.svg",
+  javascript: "/icons/express.svg",
 };
