df9efe46d3b608ee61745f6215ca0ff9a3d2f0e1
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2025-04-24 01:06:11 -0600

    blog wp

---
+++ src/types/post.d.ts
@@ -0,0 +1,11 @@
+export interface Post {
+  id: string;
+  slug: string;
+  heroImage: string;
+  title: string;
+  topic: string;
+  date: Date;
+  description: string;
+  estimatedMinutes: number;
+}
+ 
