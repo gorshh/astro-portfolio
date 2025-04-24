ec91189c15af81492122ac4aa9ac7cb8bcb24673
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2025-04-24 16:04:08 -0600

    Fix javascript icon path in topicIcons and clean up commented code

--- src/utils/topicIcons.ts
+++ src/utils/topicIcons.ts
@@ -1,22 +1,8 @@
-// export const topicIcons: Record<string, string> = {
-//   react: "⚛️",
-//   vite: "⚡",
-//   astro: "🪐",
-//   webcomponents: "🧩",
-//   expressjs: "🚂",
-//   backend: "🖥️",
-//   frontend: "🎨",
-//   typeScript: "🔷",
-//   api: "🔌",
-//   storybook: "📘",
-// };
-
-
 export const topicIcons: Record<string, string> = {
   react: "/icons/react.svg",
   vite: "/icons/vite.svg",
   astro: "/icons/astro.svg",
   webcomponents: "/icons/webcomponents.svg",
   expressjs:"/icons/express.svg",
-  javascript: "/icons/express.svg",
+  javascript: "/icons/javascript.svg",
 };
