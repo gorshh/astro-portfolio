73cfa5ac893508f50ac6147d7806cdcaa16219e6
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2025-04-25 23:10:38 -0600

    blog wp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  viewTransitions: true,
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
