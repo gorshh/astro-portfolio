3091d1d8b4ea264fbe4d8645d6f6e6e6ef33c806
jorshh <jorgecalderaperalta@gmail.com>
2025-05-18 22:29:29 -0600

    format ui

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,117 +1,131 @@
 export const showDefaultLang = false;
 
-
 export const languages = {
-  en: 'English',
-  es: 'Español',
+  en: "English",
+  es: "Español",
 };
 
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 export const ui = {
   en: {
-    'meta.description': `Full-Stack Developer with 5+ years of experience in React, Next.js, Node.js, AWS, RESTful APIs, and software architecture. Expert in testing, CI/CD, and agile development. Explore my work.`,
-    'nav.id': 'about',
-    'nav.home': 'Home',
-    'nav.about': 'About',
-    'nav.works': 'Selected Work',
-    'nav.experience': 'Experience',
-    'nav.contact': 'Contact',
-    'hero.title': `I'm a developer dedicated to pixel-perfect UI & smooth digital experiences.`,
-    'hero.cta': `Let's talk`,
-    'hero.greeting': 'Hi',
-    'about.section-name': 'About me',
-    'about.title-first-part': `👋 Hey, I'm Jorge — a developer who cares`,
-    'about.title-emphasis': 'just as much',
-    'about.title-second-part': ` about smooth animations and crisp layouts as clean, scalable code.`,
-    'about.first-description': `With 5+ years of experience, I’ve contributed to fast-moving startups and
+    "meta.description": `Full-Stack Developer with 5+ years of experience in React, Next.js, Node.js, AWS, RESTful APIs, and software architecture. Expert in testing, CI/CD, and agile development. Explore my work.`,
+    "nav.id": "about",
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.works": "Selected Work",
+    "nav.experience": "Experience",
+    "nav.contact": "Contact",
+    "hero.title": `I'm a developer dedicated to pixel-perfect UI & smooth digital experiences.`,
+    "hero.cta": `Let's talk`,
+    "hero.greeting": "Hi",
+    "about.section-name": "About me",
+    "about.title-first-part": `👋 Hey, I'm Jorge — a developer who cares`,
+    "about.title-emphasis": "just as much",
+    "about.title-second-part": ` about smooth animations and crisp layouts as clean, scalable code.`,
+    "about.first-description": `With 5+ years of experience, I’ve contributed to fast-moving startups and
       large-scale platforms. My sweet spot is modern frontend development
       (React, Astro, TypeScript), but I’m just as comfortable in the backend
       when needed.`,
-    'about.first-small-description': `Designers love me because I care about details. PMs trust me because I
+    "about.first-small-description": `Designers love me because I care about details. PMs trust me because I
       hit deadlines. Engineers appreciate me because my pull requests don’t
       break things`,
-    'about.first-small-description-thinking': 'well, usually',
-    'catch-me.first-description': `Frontend, Backend and Everything in Between. Let's`,
-    'catch-me.second-description': `awesome digital products.`,
-    'catch-me.create': 'create',
-    'catch-me.cta': 'ping me a message',
-    'experience.description':'Im a Full-stack developer with 5 years of experience building high-quality, scalable web solutions. Passionate about frontend and backend technologies, with strong skills in React, TypeScript, Node.js, and cloud services. Recognized for technical leadership, clear communication, and a proactive approach to problem-solving in agile environments like SCRUM. Focused on delivering impactful digital experiences, mentoring teams, and driving projects from idea to deployment.',
-    'testimonials.title': 'Nice things people said',
+    "about.first-small-description-thinking": "well, usually",
+    "catch-me.first-description": `Frontend, Backend and Everything in Between. Let's`,
+    "catch-me.second-description": `awesome digital products.`,
+    "catch-me.create": "create",
+    "catch-me.cta": "ping me a message",
+    "experience.description":
+      "Im a Full-stack developer with 5 years of experience building high-quality, scalable web solutions. Passionate about frontend and backend technologies, with strong skills in React, TypeScript, Node.js, and cloud services. Recognized for technical leadership, clear communication, and a proactive approach to problem-solving in agile environments like SCRUM. Focused on delivering impactful digital experiences, mentoring teams, and driving projects from idea to deployment.",
+    "testimonials.title": "Nice things people said",
 
-    'testimonials.first.description': 'Jorge is a skilled frontend dev with React, TypeScript, and UX/UI expertise. He also led backend tasks, CI/CD, and Docker deployments. Great mentor and team lead, always collaborative and reliable — a strong asset to any team.',
-    'testimonials.first.name': 'Andrés Martinez',
-    'testimonials.first.role': 'NodeJs Backend Developer',
-    'testimonials.first.linkedin': 'https://www.linkedin.com/in/andrés-martinez-971ba3125/',
-    'testimonials.first.image': 'https://media.licdn.com/dms/image/v2/C4E03AQHARQV5p8gxcQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1629283731490?e=1749081600&v=beta&t=5hPHJ8l5T_rSY2hjp7g21bt8zKXNDOfcFwbqFyngkoY',
+    "testimonials.first.description":
+      "Jorge is a skilled frontend dev with React, TypeScript, and UX/UI expertise. He also led backend tasks, CI/CD, and Docker deployments. Great mentor and team lead, always collaborative and reliable — a strong asset to any team.",
+    "testimonials.first.name": "Andrés Martinez",
+    "testimonials.first.role": "NodeJs Backend Developer",
+    "testimonials.first.linkedin":
+      "https://www.linkedin.com/in/andrés-martinez-971ba3125/",
+    "testimonials.first.image":
+      "https://media.licdn.com/dms/image/v2/C4E03AQHARQV5p8gxcQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1629283731490?e=1749081600&v=beta&t=5hPHJ8l5T_rSY2hjp7g21bt8zKXNDOfcFwbqFyngkoY",
 
-    'testimonials.second.description': 'Jorge leads with clarity and builds with purpose. His ability to balance technical skill and empathy makes him a cornerstone of any team. Always willing to help, always delivering value.',
-    'testimonials.second.name': 'Luisny Ugarte',
-    'testimonials.second.role': 'Frontend Developer, Software Designer',
-    'testimonials.second.linkedin': 'https://www.linkedin.com/in/luisny-ugarte/',
-    'testimonials.second.image': 'https://media.licdn.com/dms/image/v2/D4D03AQErMqlnvguyxA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719264346082?e=1749081600&v=beta&t=5SlIEgOn8jQYtmiGCyvFydF_jQsZdf0qHbI_N0A5waM',
+    "testimonials.second.description":
+      "Jorge leads with clarity and builds with purpose. His ability to balance technical skill and empathy makes him a cornerstone of any team. Always willing to help, always delivering value.",
+    "testimonials.second.name": "Luisny Ugarte",
+    "testimonials.second.role": "Frontend Developer, Software Designer",
+    "testimonials.second.linkedin":
+      "https://www.linkedin.com/in/luisny-ugarte/",
+    "testimonials.second.image":
+      "https://media.licdn.com/dms/image/v2/D4D03AQErMqlnvguyxA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719264346082?e=1749081600&v=beta&t=5SlIEgOn8jQYtmiGCyvFydF_jQsZdf0qHbI_N0A5waM",
 
-    'testimonials.third.description': 'Wow, you have quickly mastered advanced topics, showcasing impressive technical and management skills. Your proactive approach and willingness to step up when needed truly embody the spirit of teamwork.',
-    'testimonials.third.name': 'Aveiro Antonio Ortiz',
-    'testimonials.third.role': 'Frontend Developer Sr',
-    'testimonials.third.image': 'https://api.starmeup.com/starmeup-api/v2/image/zzkNwKHHLm_png/27/27/1',
+    "testimonials.third.description":
+      "Wow, you have quickly mastered advanced topics, showcasing impressive technical and management skills. Your proactive approach and willingness to step up when needed truly embody the spirit of teamwork.",
+    "testimonials.third.name": "Aveiro Antonio Ortiz",
+    "testimonials.third.role": "Frontend Developer Sr",
+    "testimonials.third.image":
+      "https://api.starmeup.com/starmeup-api/v2/image/zzkNwKHHLm_png/27/27/1",
 
-
-    'testimonials.fourth.description': 'Working with Jorge means clarity, solutions, and growth. He leads with empathy, tackles complex issues with precision, and always delivers better outcomes than expected. A true problem-solver and team accelerator.',
-    'testimonials.fourth.name': 'Julián Morán',
-    'testimonials.fourth.role': 'Frontend Developer Jr',
-    'testimonials.fourth.linkedin': 'https://www.linkedin.com/in/julianmoran/',
-    'testimonials.fourth.image': 'https://media.licdn.com/dms/image/v2/C4E03AQFRKLObgphfMw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1541614070580?e=1749081600&v=beta&t=w-cfubfVGktRwqSH1tADoIfND9t7zFteWSaDnlWMoDM',
+    "testimonials.fourth.description":
+      "Working with Jorge means clarity, solutions, and growth. He leads with empathy, tackles complex issues with precision, and always delivers better outcomes than expected. A true problem-solver and team accelerator.",
+    "testimonials.fourth.name": "Julián Morán",
+    "testimonials.fourth.role": "Frontend Developer Jr",
+    "testimonials.fourth.linkedin": "https://www.linkedin.com/in/julianmoran/",
+    "testimonials.fourth.image":
+      "https://media.licdn.com/dms/image/v2/C4E03AQFRKLObgphfMw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1541614070580?e=1749081600&v=beta&t=w-cfubfVGktRwqSH1tADoIfND9t7zFteWSaDnlWMoDM",
   },
   es: {
-    'meta.description': 'Desarrollador Full-Stack con más de 5 años de experiencia en React, Next.js, Node.js, AWS, APIs RESTful y arquitectura de software. Experto en pruebas, CI/CD y desarrollo ágil. Explora mi trabajo.',
-    'nav.home': 'Inicio',
-    'nav.about': 'Sobre Mi',
-    'nav.works': 'Trabajos',
-    'nav.experience': 'Experiencia',
-    'nav.contact': 'Contacto',
-    'hero.title': 'Soy un desarrollador enfocado en interfaces pixel-perfect y experiencias digitales fluidas.',
-    'hero.cta': `Hablemos`,
-    'hero.greeting': 'Hola',
-    'about.section-name': 'Sobre mí',
-    'about.title-first-part': `👋 Hola, soy Jorge — un desarrollador que se preocupa`,
-    'about.title-emphasis': 'tanto',
-    'about.title-second-part': ` por las animaciones fluidas y los diseños pulidos como por escribir código limpio y escalable.`,
-    'about.first-description': `Con más de 5 años de experiencia, he contribuido en startups de rápido crecimiento y plataformas a gran escala. Mi especialidad es el desarrollo frontend moderno (React, Astro, TypeScript), aunque también me siento cómodo trabajando en el backend cuando es necesario.`,
-    'about.first-small-description': 'A los diseñadores les agrada trabajar conmigo porque cuido los detalles. Los PMs confían en mí porque cumplo con los plazos. Los ingenieros me aprecian porque mis pull requests no rompen nada',
-    'about.first-small-description-thinking': 'bueno… casi siempre',
-    'catch-me.first-description': 'Frontend, Backend y todo lo que hay en medio. Vamos a ',
-    'catch-me.create': 'crear',
-    'catch-me.second-description': `productos digitales increíbles.`,
-    'catch-me.cta': 'Enviame un mensaje',
-    'experience.description':'Soy un desarrollador full-stack versátil con 5 años de experiencia construyendo aplicaciones web robustas y centradas en el usuario. Experto en React, TypeScript, Node.js y plataformas en la nube. Destacado por su liderazgo técnico claro, ejecución ágil y entrega constante de soluciones escalables que generan impacto real.',
+    "meta.description":
+      "Desarrollador Full-Stack con más de 5 años de experiencia en React, Next.js, Node.js, AWS, APIs RESTful y arquitectura de software. Experto en pruebas, CI/CD y desarrollo ágil. Explora mi trabajo.",
+    "nav.home": "Inicio",
+    "nav.about": "Sobre Mi",
+    "nav.works": "Trabajos",
+    "nav.experience": "Experiencia",
+    "nav.contact": "Contacto",
+    "hero.title":
+      "Soy un desarrollador enfocado en interfaces pixel-perfect y experiencias digitales fluidas.",
+    "hero.cta": `Hablemos`,
+    "hero.greeting": "Hey",
+    "about.section-name": "Sobre mí",
+    "about.title-first-part": `👋 Hola, soy Jorge — un desarrollador que se preocupa`,
+    "about.title-emphasis": "tanto",
+    "about.title-second-part": ` por las animaciones fluidas y los diseños pulidos como por escribir código limpio y escalable.`,
+    "about.first-description": `Con más de 5 años de experiencia, he contribuido en startups de rápido crecimiento y plataformas a gran escala. Mi especialidad es el desarrollo frontend moderno (React, Astro, TypeScript), aunque también me siento cómodo trabajando en el backend cuando es necesario.`,
+    "about.first-small-description":
+      "A los diseñadores les agrada trabajar conmigo porque cuido los detalles. Los PMs confían en mí porque cumplo con los plazos. Los ingenieros me aprecian porque mis pull requests no rompen nada",
+    "about.first-small-description-thinking": "bueno… casi siempre",
+    "catch-me.first-description":
+      "Frontend, Backend y todo lo que hay en medio. Vamos a ",
+    "catch-me.create": "crear",
+    "catch-me.second-description": `productos digitales increíbles.`,
+    "catch-me.cta": "Enviame un mensaje",
+    "experience.description":
+      "Soy un desarrollador full-stack versátil con 5 años de experiencia construyendo aplicaciones web robustas y centradas en el usuario. Experto en React, TypeScript, Node.js y plataformas en la nube. Destacado por su liderazgo técnico claro, ejecución ágil y entrega constante de soluciones escalables que generan impacto real.",
 
+    "testimonials.title": "Voces que me acompañan",
 
-    'testimonials.title': 'Voces que me acompañan',
+    "testimonials.first.description":
+      "Jorge es un desarrollador frontend con gran dominio de React, TypeScript y experiencia en UX/UI. También lideró tareas backend, CI/CD y despliegues con Docker. Un gran mentor y líder de equipo, siempre colaborativo y confiable — un gran aporte para cualquier equipo.",
+    "testimonials.first.role": "Desarrollador Backend NodeJs",
 
+    "testimonials.second.description":
+      "Jorge lidera con claridad y construye con propósito. Su capacidad para equilibrar habilidad técnica y empatía lo convierte en una pieza clave en cualquier equipo. Siempre dispuesto a ayudar y aportar valor.",
+    "testimonials.second.role": "Desarrollador Frontend, Diseñador de Software",
 
-    'testimonials.first.description': 'Jorge es un desarrollador frontend con gran dominio de React, TypeScript y experiencia en UX/UI. También lideró tareas backend, CI/CD y despliegues con Docker. Un gran mentor y líder de equipo, siempre colaborativo y confiable — un gran aporte para cualquier equipo.',
-    'testimonials.first.role': 'Desarrollador Backend NodeJs',
+    "testimonials.third.description":
+      "Wow, dominaste temas avanzados rápidamente, demostrando habilidades técnicas y de gestión impresionantes. Tu actitud proactiva y disposición para asumir retos reflejan verdaderamente el espíritu de trabajo en equipo.",
+    "testimonials.third.role": "Desarrollador Frontend Sr",
 
-    'testimonials.second.description': 'Jorge lidera con claridad y construye con propósito. Su capacidad para equilibrar habilidad técnica y empatía lo convierte en una pieza clave en cualquier equipo. Siempre dispuesto a ayudar y aportar valor.',
-    'testimonials.second.role': 'Desarrollador Frontend, Diseñador de Software',
-
-    'testimonials.third.description': 'Wow, dominaste temas avanzados rápidamente, demostrando habilidades técnicas y de gestión impresionantes. Tu actitud proactiva y disposición para asumir retos reflejan verdaderamente el espíritu de trabajo en equipo.',
-    'testimonials.third.role': 'Desarrollador Frontend Sr',
-
-    'testimonials.fourth.description': 'Trabajar con Jorge significa claridad, soluciones y crecimiento. Lidera con empatía, resuelve problemas complejos con precisión y siempre entrega resultados mejores de lo esperado. Un verdadero solucionador de problemas y motor para el equipo.',
-    'testimonials.fourth.role': 'Desarrollador Frontend Jr',
-
+    "testimonials.fourth.description":
+      "Trabajar con Jorge significa claridad, soluciones y crecimiento. Lidera con empatía, resuelve problemas complejos con precisión y siempre entrega resultados mejores de lo esperado. Un verdadero solucionador de problemas y motor para el equipo.",
+    "testimonials.fourth.role": "Desarrollador Frontend Jr",
   },
 } as const;
 
-
-
 export const routes = {
   en: {
-    'services': 'leistungen',
+    services: "leistungen",
   },
   es: {
-    'services': 'prestations-de-service',
+    services: "prestations-de-service",
   },
-}
+};
+
